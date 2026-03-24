2b9a5fbe9649798ae1bbffaff04d861bed37c71e
DeokJeong <sp10051993@gmail.com>
2026-03-24 18:36:18 +0900

    riot API 연결 관련 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@ import { defineNuxtConfig } from 'nuxt/config';
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: [ '~/assets/common.css' ]
+  css: [ '~/assets/common.css' ],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://api.example.com'
+    }
+  }
 })
